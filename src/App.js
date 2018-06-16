29bbcc2c4a46eba820e399985e3ab6e9c2802f23
Wee Leong Ong <wl.ong@outlook.com>
2018-06-16 15:52:12 +0800

    remove-default-html-code branch
    Removed logo.svg, remove default codes in app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <h1 style={styles.introStyle}>This is a Youtube Clone site and is under development.</h1>
     );
+  }
+}
+
+const styles = {
+  introStyle: {
+    display: 'flex',
+    height: '720px',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 }
 
