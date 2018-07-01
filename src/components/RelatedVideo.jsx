1229c9d7ad0b54d9d82685b84eab6a958767b29d
kuromoka <kuromokapb@gmail.com>
2018-07-01 23:51:59 +0900

    Displayed videos with GridList

--- src/components/RelatedVideo.jsx
+++ src/components/RelatedVideo.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import YoutubeApi from '../modules/YoutubeApi';
 
 export default class RelatedVideo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: [],
+      items: [],
     };
   }
 
@@ -18,15 +22,30 @@ export default class RelatedVideo extends Component {
     };
     const ya = new YoutubeApi('search', params);
     ya.call().then(result => {
-      console.log(result);
+      this.setState({items: result['data']['items']});
     }).catch(error => {
       console.log(error);
     });
   }
 
   render() {
+    console.log(this.state.items);
     return (
       <div>
+        <GridList cellHeight={180} cols={1} style={{ width: '320px' }}>
+          <GridListTile key="Subheader" cols={1} style={{ height: 'auto' }}>
+            <ListSubheader component="div">December</ListSubheader>
+          </GridListTile>
+          {this.state.items.map(item => (
+            <GridListTile key={item.snippet.title}>
+              <img src={item.snippet.thumbnails.medium.url} alt={item.snippet.title} />
+              <GridListTileBar
+                title={item.snippet.title}
+                subtitle={item.snippet.channelTitle}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
       </div>
     );
   }
