2d494043fb5751b1f3c419beac843ff97220f1e6
denibulkashvili <denibulkashvili@gmail.com>
2018-07-05 20:33:46 +0800

    Changed About component import name to work with #16 PR

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SearchBar from './SearchBar';
-import About from './AboutPage';
+import About from './About';
 
 const Main = () => (
   <main>
