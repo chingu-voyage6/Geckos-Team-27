39244c5c81637782ab5432d431c563fc735b18e4
kuromoka <kuromokapb@gmail.com>
2018-07-07 13:57:32 +0900

    Fix how main grid separating

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Input, Button } from '@material-ui/core';
+import { Input, Button, Grid, Typography } from '@material-ui/core';
 import YoutubeApi from '../modules/YoutubeApi';
 
 export default class SearchBar extends Component {
@@ -28,23 +28,30 @@ export default class SearchBar extends Component {
 
     return (
       <div>
-        <form onSubmit={this.onFormSubmit.bind(this)}>
-          <Input 
-            placeholder="Search" 
-            fullWidth 
-            style={{paddingLeft: '3px'}}
-            type="search" 
-            onChange={event => this.setState({ value: event.target.value })}
-            value={this.state.value}
-          />
-          <Button 
-            type="submit"
-            variant="contained"
-            style={buttonStyle} 
-          >
-          Search
-          </Button>
-        </form>
+        <Grid container justify="center" alignItems="center" style={{marginTop: '200px', textAlign: 'center'}}>
+          <Grid item xs={8}>
+            <Typography align="center" variant="headline">
+              This is a Youtube Clone site and is under development.
+            </Typography>
+            <form onSubmit={this.onFormSubmit.bind(this)}>
+              <Input 
+                placeholder="Search" 
+                fullWidth 
+                style={{paddingLeft: '3px'}}
+                type="search" 
+                onChange={event => this.setState({ value: event.target.value })}
+                value={this.state.value}
+              />
+              <Button 
+                type="submit"
+                variant="contained"
+                style={buttonStyle} 
+              >
+              Search
+              </Button>
+            </form>
+          </Grid>
+        </Grid>
       </div>
     );
   }
