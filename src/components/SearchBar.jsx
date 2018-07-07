aef9defe8fc666d7203ff4fe2a9cdd8c1da57932
kuromoka <kuromokapb@gmail.com>
2018-07-07 14:43:03 +0900

    Fix styles and move state initialization

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,15 +3,20 @@ import { Input, Button, Grid, Typography } from '@material-ui/core';
 import YoutubeApi from '../modules/YoutubeApi';
 
 export default class SearchBar extends Component {
-  state = { value: '' };
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  }
 
   onFormSubmit(event) {
     event.preventDefault();
     const params = {
-        maxResults: '20',
-        part: 'snippet',
-        q: this.state.value,
-        type: 'video, playlist',
+      maxResults: '20',
+      part: 'snippet',
+      q: this.state.value,
+      type: 'video, playlist',
     };
     const ya = new YoutubeApi('search', params);
     ya.call().then(result => {
@@ -24,16 +29,29 @@ export default class SearchBar extends Component {
   }
 
   render() {
-    const { buttonStyle } = styles;
+    const styles = {
+      container: {
+        marginTop: '150px',
+      },
+      form: {
+        marginTop: '30px',
+        textAlign: 'center', 
+      },
+      buttonStyle: {
+        marginTop: '10px',
+        backgroundColor: '#2196f3', 
+        color: '#fff',
+      },
+    };
 
     return (
       <div>
-        <Grid container justify="center" alignItems="center" style={{marginTop: '200px', textAlign: 'center'}}>
+        <Grid container justify="center" alignItems="center" style={styles.container}>
           <Grid item xs={8}>
-            <Typography align="center" variant="headline">
+            <Typography align="center" variant="display1">
               This is a Youtube Clone site and is under development.
             </Typography>
-            <form onSubmit={this.onFormSubmit.bind(this)}>
+            <form onSubmit={this.onFormSubmit.bind(this)} style={styles.form}>
               <Input 
                 placeholder="Search" 
                 fullWidth 
@@ -45,7 +63,7 @@ export default class SearchBar extends Component {
               <Button 
                 type="submit"
                 variant="contained"
-                style={buttonStyle} 
+                style={styles.buttonStyle}
               >
               Search
               </Button>
@@ -56,11 +74,3 @@ export default class SearchBar extends Component {
     );
   }
 }
-
-const styles = {
-  buttonStyle: {
-    marginTop: '10px',
-    backgroundColor: '#2196f3', 
-    color: '#fff'
-  }
-};
