94994b46a7871ccde7c0145c23e3c06e8b2e1f8d
kuromoka <kuromokapb@gmail.com>
2018-07-20 00:21:45 +0900

    Add video infomation

--- src/components/VideoViewer.jsx
+++ src/components/VideoViewer.jsx
@@ -1,25 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Typography } from '@material-ui/core';
 import YouTube from 'react-youtube';
+import YoutubeApi from '../modules/YoutubeApi';
 
-const opts = {
-  height: '390',
-  width: '640',
-  playerVars: {
-    autoplay: 0
+export default class VideoViewer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      video: {
+        title: '',
+        description: '',
+      },
+    };
   }
-};
 
-const onReady = event => {
-  event.target.pauseVideo();
-};
+  componentDidMount() {
+    const params = {
+      part: 'snippet',
+      id: this.props.videoId,
+    };
+    const ya = new YoutubeApi('videos', params);
+    ya.call().then(result => {
+      const video = result['data']['items'][0]['snippet'];
+      this.setState({
+        video: {
+          title: video['title'],
+          description: video['description'],
+        }
+      });
+    }).catch(error => {
+      console.log(error);
+    });
+  }
 
-const VideoViewer = ({ videoId }) => {
-  return (
-    <div>
-      <YouTube videoId={videoId} opts={opts} onReady={onReady} />
-    </div>
-  );
-};
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        autoplay: 0
+      }
+    };
 
-export default VideoViewer;
+    const onReady = event => {
+      event.target.pauseVideo();
+    };
+
+    return (
+      <div>
+        <YouTube videoId={this.props.videoId} opts={opts} onReady={onReady} />
+        <Typography variant="title">
+          {this.state.video.title}
+        </Typography>
+        <Typography variant="body1">
+          {this.state.video.description}
+        </Typography>
+      </div>
+    );
+  }
+}
