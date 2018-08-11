7d33699c4028afc0631dd23205c1886526fa6a46
denibulkashvili <denibulkashvili@gmail.com>
2018-08-11 17:47:36 +0300

    Apply breakpoints and hidden to make layout respond better on small screen devices

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,70 +2,75 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { 
-    Button,
     Card, 
     CardContent, 
     CardMedia,
-    Grid, 
+    Hidden, 
     Typography 
 } from '@material-ui/core';
 import { Link } from 'react-router-dom'; 
 
 
-const styles = {
+const styles = theme => ({
+  wrapper: {
+    display: 'flex',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
+  },
   card: {
     display: 'flex',
+    marginTop: 20,
   },
   details: {
     display: 'flex',
     flexDirection: 'column',
-    marginLeft: 30,
   },
   content: {
     flex: '1 0 auto',
   },
   cover: {
-    width: 180,
-    height: 120,
-    margin: 20,
+    width: 320,
+    height: 180,
+    [theme.breakpoints.down('sm')]: {
+      minWidth: '75vw',
+      minHeight: '75vw',
+    },
   },
-  
-};
+});
 
 function SearchResults(props) {
   const { classes } = props;
 
   let videoListContent;
   const videos = props.videos;
-  // console.log(props.videos);
+  console.log(props.videos);
   if(videos) {
     videoListContent = (
       <div>
         {videos.map((vid, i) => (
           <Card className={classes.card} key={i}>
-            <Grid container>
-              <Grid item xs={3}>
-                <Link to={`/watch/${vid.id.videoId}`} target="_blank">
-                  <CardMedia
-                      className={classes.cover}
-                      image={vid.snippet.thumbnails.medium.url}
-                      title={vid.snippet.title}
-                    />
-                </Link>
-              </Grid>
-              <Grid item xs>
-                <div className={classes.details}>
-                  <CardContent className={classes.content}>
+            <div className={classes.wrapper}>
+              <Link to={`/watch/${vid.id.videoId}`} target="_blank">
+                <CardMedia
+                  className={classes.cover}
+                  image={vid.snippet.thumbnails.high.url}
+                  title={vid.snippet.title}
+                />
+              </Link>
+              <div className={classes.details}>
+                <CardContent className={classes.content}>
                   <Link to={`/watch/${vid.id.videoId}`} target="_blank" style={{ textDecoration: 'none' }}>
-                    <Typography variant="headline" > {vid.snippet.title} </Typography>
+                    <Typography variant="headline" gutterBottom > {vid.snippet.title} </Typography>
                   </Link>
+                  <Hidden smDown>
                     <Typography variant="subheading" color="textSecondary">
                       {vid.snippet.description}
                     </Typography>
-                  </CardContent>
-                </div>
-              </Grid>
-            </Grid>
+                  </Hidden>
+                </CardContent>
+              </div>
+            </div>
           </Card>
         ))}
       </div>
