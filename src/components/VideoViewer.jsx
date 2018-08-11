d71728d2ab7e3a876a12340411555db84cd6534f
denibulkashvili <denibulkashvili@gmail.com>
2018-08-11 21:57:25 +0300

    revert

--- src/components/VideoViewer.jsx
+++ src/components/VideoViewer.jsx
@@ -35,30 +35,12 @@ export default class VideoViewer extends Component {
 
   render() {
     const opts = {
-      height: 390,
-      width: 640,
+      height: '390',
+      width: '640',
       playerVars: {
         autoplay: 0
       }
     };
-
-    const styles = theme => ({
-      videoWrapper: {
-        position: 'relative',
-        paddingBottom: '56.25%', /* 16:9 */
-        paddingTop: 25,
-        height: 0,
-        overflow: 'hidden'
-      },
-      iframe: {
-        width: 640,
-        height: 320,
-        [theme.breakpoints.down('sm')]: {
-          width: 480,
-          height: 240
-        }
-      }
-    });
 
     const onReady = event => {
       event.target.pauseVideo();
@@ -67,11 +49,8 @@ export default class VideoViewer extends Component {
     return (
       <div>
         <Grid container justify="center" alignItems="center">
-          <Grid item md={8} sm={12} height="990">
+          <Grid item xs={8}>
             <YouTube videoId={this.props.videoId} opts={opts} onReady={onReady} />
-            <div className={styles.videoWrapper}> 
-              <iframe width="640" height="390" className={styles.iframe} src={`https://www.youtube.com/embed/${this.props.videoId}`} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            </div>
             <Typography gutterBottom variant="headline" component="h2">
               {this.state.video.title}
             </Typography>
@@ -84,4 +63,3 @@ export default class VideoViewer extends Component {
     );
   }
 }
-
